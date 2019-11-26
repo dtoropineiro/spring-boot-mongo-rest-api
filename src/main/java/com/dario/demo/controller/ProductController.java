ee5bf587e44ab10afa816e01b0e943894cf0ac10
dario <dtoropineiro@gmail.com>
2019-11-26 19:21:38 -0300

    added get by id and delete methods

--- src/main/java/com/dario/demo/controller/ProductController.java
+++ src/main/java/com/dario/demo/controller/ProductController.java
@@ -1,9 +1,12 @@
 package com.dario.demo.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,4 +33,15 @@ public class ProductController {
 		return productRepository.findAll();
 	}
 
+	@GetMapping("/getProducts/{id}")
+	public Optional<Product> getProductByID(@PathVariable Integer id){
+		
+		return productRepository.findById(id);
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	public String deleteProductByID(@PathVariable Integer id){
+		productRepository.deleteById(id);
+		return "Added product with id: " + id;
+	}
 }
